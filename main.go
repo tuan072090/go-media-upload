fcfe974ce83b98f68e0600e1bf7dfae0cdc8f833
loiduong16 <loiduong1997@gmail.com>
2019-11-13 17:13:06 +0700

    add route upload file meete

--- main.go
+++ main.go
@@ -40,14 +40,19 @@ var monthStr = month.String()
 var dayStr = strconv.Itoa(day)
 
 var uploadPath = "upload/" + yearStr + "/" + monthStr + "/" + dayStr
+var meetePath = "meete/" + yearStr + "/" + monthStr + "/" + dayStr
 
 func main() {
 	fmt.Println("Listen on port ", PORT)
 
 	http.HandleFunc("/up", uploadFileHandler())
+	http.HandleFunc("/meete", uploadFileMeete())
 
 	fs := http.FileServer(http.Dir(uploadPath))
 	http.Handle("/files/", http.StripPrefix("/files", fs))
+	//meete
+	mt := http.FileServer(http.Dir(meetePath))
+	http.Handle("/mtfiles/", http.StripPrefix("/mtfiles", mt))
 
 	// log.Print("Server started on localhost:8080, use /upload for uploading files and /files/{fileName} for downloading")
 	log.Fatal(http.ListenAndServe(":"+PORT, nil))
@@ -116,6 +121,108 @@ func uploadFileHandler() http.HandlerFunc {
 
 		fullFileName := fileName + fileEndings[0]
 		newPath := filepath.Join(uploadPath, fullFileName)
+		//fmt.Printf("FileType: %s, File: %s\n", detectedFileType, newPath)
+
+		// write file
+		newFile, err := os.Create(newPath)
+		if err != nil {
+			//fmt.Println(err)
+			renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
+			return
+		}
+		defer newFile.Close() // idempotent, okay to call twice
+		if _, err := newFile.Write(fileBytes); err != nil || newFile.Close() != nil {
+			renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
+			return
+		}
+
+		url := baseLocalUrl + "/files/" + fullFileName
+
+		//	set url tùy môi trường
+		if env == "development" {
+			url = baseDevUrl + "/files/" + fullFileName
+		} else if env == "production" {
+			url = baseProdUrl + "/files/" + fullFileName
+		}
+
+		//	set response payload
+		res := Response{url}
+
+		js, err := json.Marshal(res)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+	})
+}
+
+//upload file meete
+func uploadFileMeete() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// fmt.Println("request vo.....", (*r).Method)
+
+		//	config CORS
+		setupResponse(&w, r)
+		if (*r).Method == "OPTIONS" {
+			return
+		}
+
+		// check folder exist and create folder
+		if _, err := os.Stat(meetePath); err != nil {
+			if os.IsNotExist(err) {
+				// file does not exist
+				createDirectory(meetePath)
+			} else {
+				// other error
+
+			}
+		}
+		// validate file size
+		// fmt.Println("maxUploadSize", maxUploadSize)
+
+		r.Body = http.MaxBytesReader(w, r.Body, maxUploadSize)
+		if err := r.ParseMultipartForm(maxUploadSize); err != nil {
+			renderError(w, "FILE_TOO_BIG", http.StatusBadRequest)
+			return
+		}
+
+		// parse and validate file and post parameters
+		file, _, err := r.FormFile("file")
+		if err != nil {
+			renderError(w, "INVALID_FILE", http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
+		fileBytes, err := ioutil.ReadAll(file)
+		if err != nil {
+			renderError(w, "INVALID_FILE", http.StatusBadRequest)
+			return
+		}
+
+		// check file type, detectcontenttype only needs the first 512 bytes
+		detectedFileType := http.DetectContentType(fileBytes)
+		switch detectedFileType {
+		case "image/jpeg", "image/jpg":
+		case "image/gif", "image/png":
+			// case "application/pdf":
+			break
+		default:
+			renderError(w, "INVALID_FILE_TYPE", http.StatusBadRequest)
+			return
+		}
+		fileName := randToken(5)
+		fileEndings, err := mime.ExtensionsByType(detectedFileType)
+		if err != nil {
+			renderError(w, "CANT_READ_FILE_TYPE", http.StatusInternalServerError)
+			return
+		}
+
+		fullFileName := fileName + fileEndings[0]
+		newPath := filepath.Join(meetePath, fullFileName)
 		//fmt.Printf("FileType: %s, File: %s\n", detectedFileType, newPath)
 
 		// write file
