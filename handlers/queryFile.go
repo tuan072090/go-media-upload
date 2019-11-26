07cac86e7adb34771528915759d0e2a14ada3804
Tuan Nguyen <tuannm072090@gmail.com>
2019-11-26 14:26:28 +0700

    add go mod, resize image meete:1024, rebateton: 2048

---
+++ handlers/queryFile.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"net/http"
+	"strings"
+)
+
+func HandleQueryFile() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestUri := r.RequestURI
+		filePath := strings.TrimPrefix(requestUri, "/files")
+
+		//w.Write(fs)
+		w.Header().Set("Cache-Control", "max-age=604800")
+		http.ServeFile(w, r, "upload"+filePath)
+	})
+}
